72e18092ab3aa545a28d844ac987adb01aba3d0a
Sockshead <juancapoar@gmail.com>
2018-04-21 15:35:28 -0500

    asx

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 window.onload = function() {
 
-    var opcion = div => {
-        document.getElementById(div)
-    };
-    if (opcion("denomFrac1").value === 0 || opcion("denomFrac2").value === 0) {
-        alert("El denominador del fraccionario no puede ser 0");
-    }
-    while (parseInt(opcion("denomFrac1").value) != 0) {
-        let Numerador1 = parseInt(opcion("numFrac1").value);
-        let Denominador1 = parseInt(opcion("denomFrac1").value);
-    }
-    while (parseInt(opcion("denomFrac2").value) != 0 != 0) {
-        let Numerador2 = parseInt(opcion("numFrac2").value);
-        let Denominador2 = parseInt(opcion("denomFrac2").value);
-    }
+    var opcion = div => document.getElementById;
+
+
 
     /*const Suma = (Frac1, Frac2) => {
 
@@ -21,14 +10,14 @@ window.onload = function() {
     const Resta = (Frac1, Frac2) => {
 
     };*/
-    const Mult = (Frac1, Frac2) => {
-        var numRes = Numerador1 * Numerador2;
-        var denomRes = Denominador1 * Denominador2;
+    const Mult = (a, b, c, d) => {
+        var numRes = a * b;
+        var denomRes = c * d;
 
         var simplify = gcd(numRes, denomRes);
         numRes = numRes / simplify;
         denomRes = denomRes / simplify;
-        return numRes + "/" + denomRes;
+        //return numRes + "/" + denomRes;
         console.log(numRes + "/" + denomRes);
     };
     /*const Div = (Frac1, Frac2) => {
@@ -41,11 +30,25 @@ window.onload = function() {
         Resta();
     });*/
     opcion("multiplicacion").addEventListener("click", event => {
-        Mult();
+        this.validar();
+        Mult(Numerador1, Numerador2, Denominador1, Denominador2);
     });
     /*opcion("division").addEventListener("click", event => {
         Div();
     });*/
+    function validar() {
+        if (opcion("denomFrac1").value === 0 || opcion("denomFrac2").value === 0) {
+            alert("El denominador del fraccionario no puede ser 0");
+        }
+        while (parseInt(opcion("denomFrac1").value) != 0) {
+            let Numerador1 = parseInt(opcion("numFrac1").value);
+            let Denominador1 = parseInt(opcion("denomFrac1").value);
+        }
+        while (parseInt(opcion("denomFrac2").value) != 0) {
+            let Numerador2 = parseInt(opcion("numFrac2").value);
+            let Denominador2 = parseInt(opcion("denomFrac2").value);
+        }
+    }
 
     function gcd(a, b) {
         while (b !== 0) {
